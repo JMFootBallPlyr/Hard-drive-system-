9904417ca052817d426ad7d7be5f446e6e5dea86
MinEunjae <mineunjae@fastmail.fm>
2016-02-15 14:37:58 +0100

    Updated files.

--- APPR/AS/03/3.4--eGameDarlingTabellen-mine.sql
+++ APPR/AS/03/3.4--eGameDarlingTabellen-mine.sql
@@ -2,28 +2,28 @@ CREATE DATABASE IF NOT EXISTS egamedarling;
 USE egamedarling;
 
 CREATE TABLE accounts (
-    accountid VARCHAR(MAX),
-    password VARCHAR(MAX),
-    email VARCHAR(MAX),
-    avatar VARCHAR(MAX),
-    nickname VARCHAR(MAX),
+    accountid VARCHAR(30),
+    password VARCHAR(30),
+    email VARCHAR(30),
+    avatar BLOB,
+    nickname VARCHAR(30),
 
-    UNIQUE (accountid)
+    UNIQUE (accountid),
     
-    PRIMARY KEY (accountid),
+    PRIMARY KEY (accountid)
 );
 
 CREATE TABLE gamers (
-    gamerid VARCHAR(MAX),
-    name VARCHAR(MAX),
-    surname VARCHAR(MAX),
-    gender VARCHAR(MAX),
-    residence VARCHAR(MAX),
+    gamerid VARCHAR(30),
+    name VARCHAR(30),
+    surname VARCHAR(30),
+    gender VARCHAR(30),
+    residence VARCHAR(30),
     birthday DATE,
-    occupation VARCHAR(MAX),
-    PFaccountid VARCHAR(MAX),
+    occupation VARCHAR(30),
+    PFaccountid VARCHAR(30),
 
-    UNIQUE (gamerid)
+    UNIQUE (gamerid),
 
     PRIMARY KEY (PFaccountid),
     CONSTRAINT PFaccountidgamer
@@ -33,29 +33,29 @@ CREATE TABLE gamers (
 );
 
 CREATE TABLE games (
-    gameid VARCHAR(MAX),
-    gamename VARCHAR(MAX),
+    gameid VARCHAR(30),
+    gamename VARCHAR(30),
 
     PRIMARY KEY (gameid)
 );
 
 CREATE TABLE genres (
-    genreid VARCHAR(MAX),
-    genrename VARCHAR(MAX),
+    genreid VARCHAR(30),
+    genrename VARCHAR(30),
 
     PRIMARY KEY (genreid)
 );
 
 CREATE TABLE hobbies (
-    hobbyid VARCHAR(MAX),
-    hobbyname VARCHAR(MAX),
+    hobbyid VARCHAR(30),
+    hobbyname VARCHAR(30),
 
     PRIMARY KEY (hobbyid)
 );
 
 CREATE TABLE statuses (
-    statusid VARCHAR(MAX),
-    content VARCHAR(MAX),
+    statusid VARCHAR(30),
+    content VARCHAR(30),
     created TIMESTAMP,
 
     PRIMARY KEY (statusid)
@@ -64,32 +64,30 @@ CREATE TABLE statuses (
 -- -- -- CORRELATING TABLES
 
 CREATE TABLE accountsgamers (
-    PFaccountid VARCHAR(MAX),
-    PFgamerid VARCHAR(MAX),
+    PFaccountid VARCHAR(30),
+    PFgamerid VARCHAR(30),
 
     PRIMARY KEY (PFaccountid, PFgamerid),
 
-    CONSTRAINT  accgamacc
-    FOREIGN KEY (PFaccountid) accounts(accountid)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION
+    CONSTRAINT accgamacc
+    FOREIGN KEY (PFaccountid) REFERENCES accounts(accountid)
+    ON DELETE CASCADE,
 
     CONSTRAINT accgamgam
-    FOREIGN KEY (PFgamerid) gamers(gamerid)
+    FOREIGN KEY (PFgamerid) REFERENCES gamers(gamerid)
     ON DELETE CASCADE
-    ON UPDATE NO ACTION
 );
 
 CREATE TABLE gamersgames (
-    PFaccountid VARCHAR(MAX),
-    PFgameid VARCHAR(MAX),
+    PFaccountid VARCHAR(30),
+    PFgameid VARCHAR(30),
 
     PRIMARY KEY (PFaccountid, PFgameid),
 
     CONSTRAINT gamgamacc
     FOREIGN KEY (PFaccountid) REFERENCES accounts(accountid)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION
+    ON UPDATE NO ACTION,
 
     CONSTRAINT PFgamgamgam
     FOREIGN KEY (PFgameid) REFERENCES games(gameid)
@@ -97,16 +95,15 @@ CREATE TABLE gamersgames (
     ON UPDATE NO ACTION
 );
 
-CREATE TABLE gamershobbies (
-    PFgamerid VARCHAR(MAX),
-    PFhobbyid VARCHAR(MAX),
+    PFgamerid VARCHAR(30),
+    PFhobbyid VARCHAR(30),
 
     PRIMARY KEY (PFgamerid, PFhobbyid),
 
     CONSTRAINT gamhobgam
     FOREIGN KEY (PFgamerid) REFERENCES gamers(gamerid)
     ON DELETE CASCADE
-    ON UPDATE NO ACTION
+    ON UPDATE NO ACTION,
 
     CONSTRAINT gamhobhob
     FOREIGN KEY (PFhobbyid) REFERENCES hobbies(hobbyid)
@@ -115,15 +112,15 @@ CREATE TABLE gamershobbies (
 );
 
 CREATE TABLE gamesgenres (
-    PFgameid VARCHAR(MAX),
-    PFgenreid VARCHAR(MAX),
+    PFgameid VARCHAR(30),
+    PFgenreid VARCHAR(30),
 
     PRIMARY KEY (PFgameid, PFgenreid),
 
     CONSTRAINT gamgengam
     FOREIGN KEY (PFgameid) REFERENCES games(gameid)
     ON DELETE CASCADE
-    ON UPDATE NO ACTION
+    ON UPDATE NO ACTION,
 
     CONSTRAINT gamgengen
     FOREIGN KEY (PFgenreid) REFERENCES genres(genreid)
@@ -132,39 +129,18 @@ CREATE TABLE gamesgenres (
 );
 
 CREATE TABLE accountsstatuses (
-    PFaccountid VARCHAR(MAX),
-    PFstatusid VARCHAR(MAX),
+    PFaccountid VARCHAR(30),
+    PFstatusid VARCHAR(30),
 
     PRIMARY KEY (PFaccountid, PFstatusid),
 
     CONSTRAINT accstaacc
     FOREIGN KEY (PFaccountid) REFERENCES accounts(accountid)
     ON DELETE CASCADE
-    ON UPDATE NO ACTION
+    ON UPDATE NO ACTION,
 
     CONSTRAINT accstasta
     FOREIGN KEY (PFstatusid) REFERENCES statuses(statusid)
     ON DELETE CASCADE
     ON UPDATE NO ACTION
 );
-
-/*
-CREATE TABLE statusestimestamps (
-    Paccountid VARCHAR(MAX),
-    created TIMESTAMP,
-
-    PRIMARY KEY (Paccountid),
-    CONSTRAINT $
-    FOREIGN KEY () REFERENCES $()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-);
-*/
-
-/*
-PRIMARY KEY (P_schueler_id),
-CONSTRAINT F_Klassenlehrer
-FOREIGN KEY (F_Klassenlehrer) REFERENCES T_Lehrer (P_Lehrer_id)
-ON DELETE NO ACTION
-ON UPDATE NO ACTION
-*/

740a63ea09e88ecc92f1175ad9cecedfc3fbb2d8
CaptainYeowang <yeowang@fastmail.fm>
2016-04-08 12:27:11 +0200

    Added BVA website.

---
+++ BVA/SafeShield/www/index.php
@@ -0,0 +1,13 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <?php include ('header.html'); ?><br><br>
+</head>
+
+<body>
+    <?php include ('opener.html'); ?><br><br>
+    <?php include ('footer.html'); ?><br><br>
+</body>
+
+</html>
